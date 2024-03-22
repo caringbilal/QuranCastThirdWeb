73db25238b9826c9c76303a103b6073c1aff3d78
Bilal <caringbilal@gmail.com>
2024-03-22 10:22:36 +0400

    Commit on 21-MAR-24 from Office PC 3M

--- pages/index.js
+++ pages/index.js
@@ -132,7 +132,11 @@ export default function Home() {
 
           {/* Below section for connecting Wallet */}
           <div className={styles.connect}>
-            <ConnectWallet />
+            <ConnectWallet 
+              theme="dark"
+              btnTitle="Connect Wallet"
+              termsOfServiceUrl="https://qurancast.co/legal.html"
+            />
             <p>Your address: {address}</p>
             {/* I'll add terms of service and privacy policy pages
             <ConnectWallet
