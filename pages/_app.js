d4106c22fb49d2aa31eae2f1ad4496ae8773e397
Dev <caringbilal@gmail.com>
2024-04-18 14:56:29 +0400

    contract with USDC on Base Sepolia - not working - 18-APR-24

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import { ThirdwebProvider } from '@thirdweb-dev/react';
+import { ThirdwebProvider, useChain } from '@thirdweb-dev/react';
 import '../styles/globals.css';
-import { Sepolia, Arbitrum, Optimism, Base, Binance } from '@thirdweb-dev/chains';
+import { Sepolia, Arbitrum, Optimism, Base, Binance, ChainId } from '@thirdweb-dev/chains';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -8,15 +8,17 @@ import { Sepolia, Arbitrum, Optimism, Base, Binance } from '@thirdweb-dev/chains
 //const activeChain = 'sepolia';
 
 function MyApp({ Component, pageProps }) {
+	//const { activeChainNew } = useChain();
+
 	return (
 		<ThirdwebProvider
 			activeChain={Sepolia}
 			supportedChains={[
-				Sepolia,
-				Arbitrum,
-				Optimism,
-				Base,
-				Binance
+				Arbitrum, 
+                Base, 
+                Binance, 
+                Optimism, 
+                Sepolia
 			]}
 			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
 		>
